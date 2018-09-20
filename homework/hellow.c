726b6056e987a9bc56abd1b1a89c84ef0ff47b1a
Ludwig Robert <rludwig3@fourier.cs.iit.edu>
2018-09-20 16:17:58 -0500

    Fixed version of hellow.c

--- homework/hellow.c
+++ homework/hellow.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "mpi.h"
 
 int main(int argc, char *argv[])
@@ -14,7 +15,7 @@ int main(int argc, char *argv[])
 
     int myid;
     int size;
-    int *ranks = NULL;
+    int ranks[10];
 
 //MPI init, and get rank and size for each process
 
@@ -24,18 +25,12 @@ int main(int argc, char *argv[])
 
 //  printf("DEBUG0: I am process %d of %d\n", myid+1, size);
 
-//Initialize ranks[] on process 0 
-
-    if (myid == 0) {
-//	printf("DEBUG1\n");
-	int ranks[size];
-//	printf("DEBUG2: size: %d, rank address: %pn\n", size, &ranks);
-    }
-
 //MPI_Gather collects myid rank from each process in rank order into ranks[], cannot be called within if(myid ==0)
 //For some reason, gather multiplies the ranks by 4 no matter the number of processes.
-
-    MPI_Gather(&myid, 1, MPI_INT, &ranks, 1, MPI_INT, 0, MPI_COMM_WORLD); 
+  if (myid == 0) {
+  	int ranks[size];
+  }
+    MPI_Gather(&myid, 1, MPI_INT, ranks, 1, MPI_INT, 0, MPI_COMM_WORLD); 
 //  printf("DEBUG3\n");
 
 //Make process 0 print array in reverse order, top and rank compensate for multiplication of ranks by 4
@@ -44,13 +39,11 @@ int main(int argc, char *argv[])
 //	printf("DEBUG4\n");
 /*DEBUG
 	for (int j = 0; j < size; j++) {
-		printf("Ranks[%d]: %d\n", j, &ranks[j]);
+		printf("Ranks[%d]: %d\n", j, ranks[j]);
 	}
 */
-	for (int i = size; i > 0; i--) {
-		int top = &ranks[i];
-		int rank = top/4;
-		printf("Hello world from process %d of %d\n", rank, size);
+	for (int i = size-1 ; i >= 0; i--) {
+		printf("Hello world from process %d of %d\n", ranks[i]+1, size);
     	}
     }
     MPI_Finalize();
