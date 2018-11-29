0fa3ab4b01335008350fa1a5101e8fcc7e42911e
Ludwig Robert <rludwig3@fourier.cs.iit.edu>
2018-11-29 17:44:10 -0600

    Makefile with KSP_LIB var

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ include $(PETSC_DIR)/lib/petsc/conf/variables
 include $(SALSA_MODULES_DIR)/Make.inc
 
 
-CFLAGS = $(PETSC_INCLUDE) $(COPTFLAGS) -I$(SALSA_MODULES_DIR) $(HAVE_NMD_DEFINE) -I$(LIBNMD_INCLUDE_DIR) -I$(LIBXMLSC_INCLUDE_DIR)
+CFLAGS = $(PETSC_INCLUDE) $(COPTFLAGS) -I$(SALSA_MODULES_DIR) $(HAVE_NMD_DEFINE) -I$(LIBNMD_INCLUDE_DIR)
 
 anamod_test: anamod_test.o
-	$(CLINKER) -o anamod_test anamod_test.o \
+	$(CLINKER) -o anamod_test anamod_test.o ${PETSC_KSP_LIB}
 	-L$(SALSA_MODULES_LIB_DIR) -lsalsamodules -lothermodules \
 	$(PETSC_LIB)
--- anamod_test.c
+++ anamod_test.c
@@ -1,18 +1,19 @@
 #include <stdlib.h>
 
-#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamod.h>
-#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamodsalsamodules.h>
-
-/*#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petsc.h"
-#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petscksp.h"*/
+/*#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamod.h>*/
+/*#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamodsalsamodules.h>*/
+#include <petsc.h>
+#include <petscksp.h>
+/*#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petsc.h"*/
+/*#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petscksp.h"*/
 
 int main(int argc,char **argv)
 {
   /* Declare vars here */
   PetscErrorCode ierr;
   ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
-  ierr = AnaModInitialize(); CHKERRQ(ierr);
-  ierr = AnaModFinalize(); CHKERRQ(ierr);
+  //ierr = AnaModInitialize(); CHKERRQ(ierr);
+  //ierr = AnaModFinalize(); CHKERRQ(ierr);
   PetscFinalize();
   return 0;
 }
