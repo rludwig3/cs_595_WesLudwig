2c735b41d56bc3a0b0cfdcc94f57dd72dcb91ec0
Ludwig Robert <rludwig3@fourier.cs.iit.edu>
2018-11-29 17:13:17 -0600

    First commit

---
+++ anamod_test.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamod.h>
+#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamodsalsamodules.h>
+
+/*#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petsc.h"
+#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petscksp.h"*/
+
+int main(int argc,char **argv)
+{
+  /* Declare vars here */
+  PetscErrorCode ierr;
+  ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
+  ierr = AnaModInitialize(); CHKERRQ(ierr);
+  ierr = AnaModFinalize(); CHKERRQ(ierr);
+  PetscFinalize();
+  return 0;
+}
