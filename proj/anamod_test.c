24d98804fa795b381b0f8ee3036beaf92d868b30
Ludwig Robert <rludwig3@fourier.cs.iit.edu>
2018-12-02 16:38:32 -0600

    Makefile and anamod_test updated to match what Dr. Zhang sent, works when anamod code is commented out. Throws errors when anamod is introduced, unclear what is the problem, but it doesn't recognize the AnamodInitialize and AnamodFinalize functions

--- proj/anamod_test.c
+++ proj/anamod_test.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 
-/*#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamod.h>*/
-/*#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamodsalsamodules.h>*/
+//#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamod.h>
+//#include </home/class/fall-18/cs595/rludwig3/soft/VictorEijkhout-salsa-025c63ed4210/anamod/anamodsalsamodules.h>
 #include <petsc.h>
 #include <petscksp.h>
+#include <anamod.h>
+#include <anamodsalasamodules.h>
 /*#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petsc.h"*/
 /*#include "/home/class/fall-18/cs595/rludwig3/soft/petsc/include/petscksp.h"*/
 
@@ -12,8 +14,8 @@ int main(int argc,char **argv)
   /* Declare vars here */
   PetscErrorCode ierr;
   ierr = PetscInitialize(&argc,&argv,(char*)0,0);if (ierr) return ierr;
-  //ierr = AnaModInitialize(); CHKERRQ(ierr);
-  //ierr = AnaModFinalize(); CHKERRQ(ierr);
+  ierr = AnaModInitialize(); CHKERRQ(ierr);
+  ierr = AnaModFinalize(); CHKERRQ(ierr);
   PetscFinalize();
   return 0;
 }
